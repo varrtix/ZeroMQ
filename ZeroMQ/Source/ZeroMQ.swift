51836eea96c87540d1996a3c89d85ba0f58f6015
varrtix <tiamo_nana@outlook.com>
2020-07-05 19:53:40 +0800

    Create: The main abstraction for ZeroMQ

--- ZeroMQ/Source/ZeroMQ.swift
+++ ZeroMQ/Source/ZeroMQ.swift
@@ -6,9 +6,19 @@
 //  Copyright © 2020 VARRTIX. All rights reserved.
 //
 
+import Foundation
+
+/// The main abstraction for ZeroMQ to prevent name collisions with unrelated libraries.
 public struct ZeroMQ {
   
+  /// A private initialization to prevent instantion.
   private init() {}
   
+  /// Version of the libzmq Low-level API.
+  public static var zmqVersion: String { "4.3.2" }
   
+  /// Version of this framework.
+  public static var framewrokVersion: String {
+    Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+  }
 }
