5015f99d3d5fbdd5ba3efb4d3d1b424272188317
varrtix <tiamo_nana@outlook.com>
2020-07-05 20:58:07 +0800

    Add: GPLv3 License

--- ZeroMQ/Source/Socket.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Socket.swift
-//  ZeroMQ
-//
-//  Created by Darcy Chiu on 2020/7/5.
-//  Copyright © 2020 VARRTIX. All rights reserved.
-//
-
-import Foundation
--- ZeroMQ/Source/ZeroMQ.swift
+++ ZeroMQ/Source/ZeroMQ.swift
@@ -1,10 +1,23 @@
 //
 //  ZeroMQ.swift
-//  ZeroMQ
+//  ZeroMQ.swift
 //
-//  Created by Darcy Chiu on 2020/7/5.
-//  Copyright © 2020 VARRTIX. All rights reserved.
+//  Created by darcychiu on 2020/7/5.
 //
+//  Copyright (C) 2020 Wei Zhao <tiamo_nana@outlook.com>.
+//
+//  This file is part of the ZeroMQ.swift library. This library is free
+//  software: you can redistribute it and/or modify it under the terms of
+//  the GNU General Public License as published by the Free Software Foundation,
+//  either version 3 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the ZeroMQ.swift.  If not, see <https://www.gnu.org/licenses/>.
 
 import Foundation
 
